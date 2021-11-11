4fb67201414169a2687f41c4056b2e284b4938cb
Sebastiano Tronto <sebastiano.tronto@gmail.com>
2021-11-11 22:05:00 +0100

    Removed old files

--- old/2020-unknown-date/moves.h
+++
@@ -1,9 +0,0 @@
-/* Moves and transition tables */
-
-#ifndef MOVES_H
-#define MOVES_H
-
-void apply_move(Move m, Cube *cube);
-void init_ttables();
-
-#endif
--- old/2021-11-10-beforeremovingchecker/solve.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOLVE_H
-#define SOLVE_H
-
-#include "moves.h"
-#include "trans.h"
-
-AlgList *   solve(Cube cube, Step *step, SolveOptions *opts);
-
-#endif
