76e04a75b16a452c5021d6d1e499a15b44fc586d
Sebastiano Tronto <sebastiano@tronto.net>
2023-01-22 00:25:47 +0100

    Removed old test files

--- tests/fst_test_util.h
+++
@@ -1,7 +0,0 @@
-#include "../src/fst.h"
-
-extern char *algs[];
-
-bool equal_and_log(Cube *c, Cube *d);
-bool try_str(CubeTester f, char *algstr, char *msg);
-bool try_all_str(CubeTester f, char *msg);
